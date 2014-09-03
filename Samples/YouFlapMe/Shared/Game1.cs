b4d7292540da9ce1c22e1edee800a668141bb579
Joe Booth <joe@joebooth.com>
2014-09-02 22:47:16 -0700

    YouFlapMe - fixed build error
    added missing reference to Youbetme.Helpers
    made iOS default platform

--- Samples/YouFlapMe/Shared/Game1.cs
+++ Samples/YouFlapMe/Shared/Game1.cs
@@ -7,6 +7,7 @@
 //-----------------------------------------------------------------------------
 using System.Collections.Generic;
 using System.Linq;
+using Youbetme.Helpers;
 
 #endregion
 #region Using Statements
